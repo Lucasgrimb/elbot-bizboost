f1a6296f71a87de671ebce999143250b9be1c7cb
Lucasgrimb <108102466+Lucasgrimb@users.noreply.github.com>
2024-08-27 14:36:37 -0300

    /despertar

--- run.py
+++ run.py
@@ -15,7 +15,13 @@ from waitress import serve  # Importar waitress para usar como servidor WSGI
 
 app = create_app()
 
+@app.route('/despertar', methods=['GET'])  # Ruta que responde a solicitudes GET
+def despertar():
+    return "Hola desde mi ruta GET"
+
 if __name__ == "__main__":
     logging.info("Flask app started with Waitress")
     # Usar waitress en lugar de app.run()
     serve(app, host="0.0.0.0", port=8000)
+
+
