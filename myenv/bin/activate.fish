8ffaa74d5a6a2d8529c98b5cad6430c8fd32819d
Lucasgrimb <108102466+Lucasgrimb@users.noreply.github.com>
2024-08-27 14:24:21 -0300

    wsgi server

--- myenv/bin/activate.fish
+++ myenv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/lucasgrimberg/proyeqto/elbot/myenv"
+set -gx VIRTUAL_ENV "/Users/lucasgrimberg/proyeqto/elbot-bizboost/myenv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
--- run.py
+++ run.py
@@ -1,10 +1,21 @@
+# import logging
+
+# from app import create_app
+
+
+# app = create_app()
+
+# if __name__ == "__main__":
+#     logging.info("Flask app started")
+#     app.run(host="0.0.0.0", port=8000)
+
 import logging
-
 from app import create_app
-
+from waitress import serve  # Importar waitress para usar como servidor WSGI
 
 app = create_app()
 
 if __name__ == "__main__":
-    logging.info("Flask app started")
-    app.run(host="0.0.0.0", port=8000)
+    logging.info("Flask app started with Waitress")
+    # Usar waitress en lugar de app.run()
+    serve(app, host="0.0.0.0", port=8000)
