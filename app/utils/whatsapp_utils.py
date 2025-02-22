04d051aa07687c2a75b2a10a40195464a0bd3636
Lucasgrimb <108102466+Lucasgrimb@users.noreply.github.com>
2025-02-22 00:21:48 -0300

    es_LA

--- app/utils/whatsapp_utils.py
+++ app/utils/whatsapp_utils.py
@@ -168,7 +168,7 @@ import requests
 import logging
 from flask import current_app
 
-def send_template_message(recipient, template_name, language_code="es_ES", components=None):
+def send_template_message(recipient, template_name, language_code="es_LA", components=None):
     """
     Send a proactive WhatsApp message using a pre-approved template.
 
