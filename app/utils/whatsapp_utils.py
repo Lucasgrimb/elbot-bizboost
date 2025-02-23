36508f339942042665663bfdb0c63ce14959664e
Lucasgrimb <108102466+Lucasgrimb@users.noreply.github.com>
2025-02-23 03:55:26 -0300

    cambio template es

--- app/utils/whatsapp_utils.py
+++ app/utils/whatsapp_utils.py
@@ -168,7 +168,7 @@ import requests
 import logging
 from flask import current_app
 
-def send_template_message(recipient, template_name, language_code="en_US", components=None):
+def send_template_message(recipient, template_name, language_code="es", components=None):
     """
     Send a proactive WhatsApp message using a pre-approved template.
 
